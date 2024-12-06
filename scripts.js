b9a234171fb5002b9955113d1385d5ed6c32a2ec
Yeni Almanza <127815619+almanzayeni@users.noreply.github.com>
2024-12-06 13:01:52 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Get modal element
+var modal = document.getElementById("signupModal");
+
+// Get the button that opens the modal
+var signupBtn = document.getElementById("signupBtn");
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName("close-btn")[0];
+
+// When the user clicks on the button, open the modal
+signupBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
